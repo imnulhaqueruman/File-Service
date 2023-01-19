745bd9ec81e19038008b2f39a725e965b83e5446
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2023-01-19 23:10:40 +0600

    aws config setup

--- controller/fileUploader.js
+++ controller/fileUploader.js
@@ -4,3 +4,16 @@ const aws = require('aws-sdk')
 const fs = require('fs')
 const path = require("path");
 const sharp = require('sharp');
+
+
+exports.Uploader= async(res,req,next) =>{
+    aws.config.setPromisesDependency();
+    aws.config.update({
+        accessKeyId:process.env.Access_key,
+        secretAccessKey:process.env.Secret_key,
+        endpoint:process.env.End_Point_Url,
+        sslEnabled: false,
+        forcePathStyle:true
+       
+    })
+}
