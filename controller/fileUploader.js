ea1da7ee50a8d6c4152fe989d6f230fd965224da
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2023-01-19 23:30:15 +0600

    controller done

--- controller/fileUploader.js
+++ controller/fileUploader.js
@@ -20,4 +20,31 @@ exports.Uploader= async(res,req,next) =>{
     const s3 = new aws.S3();
     const date = new Date();
     const rowImage = `${req.file.originalname}`;
+    var params_rowImage = {
+        ACL: 'public-read',
+        Bucket:process.env.Bucket_Name,
+        Body: fs.createReadStream(req.file.path),
+        ContentType: req.file.mimetype,
+        Key: `avatar/${rowImage} `
+      };
+      // console.log(params)
+      await s3.upload(params_rowImage,(err,data) =>{
+        if(err){
+            console.log('Error occured while trying to upload to s3 bucket', err)
+        }
+        if (data) {
+            fs.unlinkSync(req.file.path); // Empty temp folder
+            const locationUrl = data.Location;
+            // console.log(data)
+            let image = new Images({avatar: locationUrl, uid:uid });
+            image
+              .save()
+              .then(img => {
+                res.json({ message: 'image saved successfully', img });
+              })
+              .catch(err => {
+                console.log('Error occured while trying to save to DB');
+              });
+          }
+      })
 }
